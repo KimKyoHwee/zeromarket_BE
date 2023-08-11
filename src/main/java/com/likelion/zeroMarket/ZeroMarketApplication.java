2389e00459035c191b16c7ecbd09b0b9fca17fe7
김교휘 <hwee000@naver.com>
2023-08-11 18:27:12 +0900

    application fix

--- src/main/java/com/likelion/zeroMarket/ZeroMarketApplication.java
+++ src/main/java/com/likelion/zeroMarket/ZeroMarketApplication.java
@@ -2,12 +2,26 @@ package com.likelion.zeroMarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ZeroMarketApplication {
+	public static final String APPLICATION_LOCATIONS =
+			"spring.config.location="
+					+ "classpath:application.yml";
 
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ZeroMarketApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+	}
+
+
+	/*
 	public static void main(String[] args) {
 		SpringApplication.run(ZeroMarketApplication.class, args);
 	}
+
+	 */
 
 }
