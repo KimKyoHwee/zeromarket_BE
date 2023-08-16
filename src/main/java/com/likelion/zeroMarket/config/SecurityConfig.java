0f9a8a431a6c21ef4e97aa4e27f37f9700fe8b4f
김교휘 <hwee000@naver.com>
2023-08-16 18:35:22 +0900

    ZeroMarket BE 1.0.0

--- src/main/java/com/likelion/zeroMarket/config/SecurityConfig.java
+++ src/main/java/com/likelion/zeroMarket/config/SecurityConfig.java
@@ -1,11 +1,23 @@
 package com.likelion.zeroMarket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig implements WebMvcConfigurer {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+        ;
+        return http.build();
+    }
 
     // allowedOrigins에 프론트 서버 도메인이 들어가면 될거 같은 느낌?
     // 현재는 전부 허용
