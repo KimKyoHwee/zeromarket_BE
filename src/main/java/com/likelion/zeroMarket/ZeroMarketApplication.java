735c55fbd0c3e25ed46bedd3adffed82a567ff05
김교휘 <hwee000@naver.com>
2023-08-18 19:43:18 +0900

    ZeroMarket BE 1.0.5

--- src/main/java/com/likelion/zeroMarket/ZeroMarketApplication.java
+++ src/main/java/com/likelion/zeroMarket/ZeroMarketApplication.java
@@ -1,9 +1,13 @@
 package com.likelion.zeroMarket;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@OpenAPIDefinition(servers = {@Server(url="/", description = "Default Server URL")})
 
 @SpringBootApplication
 @EnableJpaAuditing  //JpaAuditing 기능 사용
